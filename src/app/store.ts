97743df9d85e6832365d52009083f7702fa4a229
BlueAlien99 <45181394+BlueAlien99@users.noreply.github.com>
2021-11-21 21:01:15 +0100

    removed unused files

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,20 +1,18 @@
 import { configureStore, AnyAction, combineReducers } from '@reduxjs/toolkit';
 import { combineEpics, createEpicMiddleware } from 'redux-observable';
 import aliceDataReducer from 'features/aliceData/aliceDataSlice';
-import counterReducer, { incrementIfOddEpic } from 'features/counter/counterSlice';
 import screenshotReducer, { screenshotTakenEpic } from 'features/screenshot/screenshotSlice';
 import stylesReducer from 'features/styles/stylesSlice';
 import webSocketReducer from 'features/webSocket/webSocketSlice';
 
 const rootReducer = combineReducers({
     aliceData: aliceDataReducer,
-    counter: counterReducer,
     screenshot: screenshotReducer,
     styles: stylesReducer,
     webSocket: webSocketReducer,
 });
 
-const rootEpic = combineEpics(incrementIfOddEpic, screenshotTakenEpic);
+const rootEpic = combineEpics(screenshotTakenEpic);
 
 const epicMiddleware = createEpicMiddleware<AnyAction, AnyAction, ReturnType<typeof rootReducer>>();
 
